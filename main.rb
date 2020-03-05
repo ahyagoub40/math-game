f053265904fedfc61552b70aa97484ebd086f426
Ahmed Yagoub <ahyagoub40@gmail.com>
2020-03-05 23:04:49 +0000

    got the functionality working, questions and answers not displayed

--- main.rb
+++ main.rb
@@ -1,9 +1,5 @@
 require './player'
-require 'questions'
-require './messages'
-require './answers'
-require './life'
-require './input_output'
+require './questions'
 
 # puts @question
 # puts @input
@@ -12,25 +8,33 @@ class Game
   def initialize(p1, p2)
     @player1 = Player.new(p1)
     @player2 = Player.new(p2)
+    @turn = @player1
   end
   def message
-    if stop?
-      puts '---- Game Over ----\nGood bye!'
-    else
+    if go?
       puts'---- New Turn ----'
+    else
+      puts '---- Game Over ----\nGood bye!'
     end
   end
   def run
-    puts "#{@player1.name}: #{@question}"
-    gets.chomp
-    puts "#{@player1.name}: #{@answer}"
-    puts @player1.score
-    message
+    while go?
+      puts "#{@turn.name}: #{@question}"
+      user_input = gets.chomp
+      puts "#{@turn.name}: #{@answer}"
+      @turn.lose_life if user_input != @answer 
+      puts @turn.score
+      message
+      switch_turn?
+    end
   end
-  def stop?
+  def go?
     @player1.alive? && @player2.alive?
+  end
+  def switch_turn?
+    @turn = (@turn == @player1) ? @player2 : @player1
   end
 end
 
-game = Game.new("Ahmed", "Anton")
-game.run()
+game = Game.new("Ahmed", "Shannon")
+game.run
--- player.rb
+++ player.rb
@@ -1,6 +1,7 @@
 class Player
+  attr_accessor :name
   def initialize(name)
-    @name = name
+  self.name = name
     @life = 3
   end
   def lose_life
